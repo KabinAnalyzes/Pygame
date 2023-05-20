73150fa69c263e7aff67381bf38dc83c99c12342
Kevin Loun <86264301+KabinAnalyzes@users.noreply.github.com>
2023-05-19 22:14:35 -0700

    Basic Graphics

--- Main.py
+++ Main.py
@@ -6,11 +6,17 @@ screen = pygame.display.set_mode((800,800))
 pygame.display.set_caption("A NapN RPG")
 clock = pygame.time.Clock()
 
+bg_surface = pygame.image.load("graphics/bg.png").convert()
+player_surface = pygame.image.load("graphics/player.png").convert()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    
+    screen.blit(bg_surface,(0,0))
+    screen.blit(player_surface,(80,200))
     
     pygame.display.update()
     clock.tick(60)
